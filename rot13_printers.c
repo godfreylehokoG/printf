641fb4b2e91e9e5517cbb905d3dd04883d22c33a
root <ramonbritton@gmail.com>
2022-04-20 12:31:19 -0700

    update on bi and rot13_printers.c

---
+++ rot13_printers.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * rot_13 - prints the replacement 13th letter for a particular letter
+ * @rot: list being passed
+ * Return: count of chars
+ */
+int rot_13(va_list rot)
+{
+	int a;
+	int count;
+	char *c;
+
+	a = count = 0;
+	c = va_arg(rot, char *);
+
+	if (c == NULL)
+		return (-1);
+	while (c[a] != '\0')
+	{
+		if ((s[c] >= 'a' && s[c] <= 'z') ||
+				(s[c] > 'A' && s[c] <= 'Z'))
+		{
+			if ((s[c] >= 'n' && s[c] <= 'z') ||
+				(s[c] >= 'N' && s[c] <= 'Z'))
+				count = count + _putchar(s[c] - 13);
+			else
+				count = count + _putchar(s[c] + 13);
+		}
+		else
+			count = count + _putchar(s[c]);
+		c++;
+	}
+	return (count);
+}
